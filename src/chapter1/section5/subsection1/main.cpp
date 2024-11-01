fd495864ba9f3dd373a09c71bfb05823e7590581
mathusanm6 <hi@mathusan.me>
2024-11-01 12:04:46 +0100

    feat: add chapter1 section5 subsection1 (book sales)

---
+++ src/chapter1/section5/subsection1/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "Sales_item.h"
+
+Sales_item init_new_item()
+{
+    Sales_item book;
+
+    // read ISBN, number of copies sold, and sales price
+    std::cin >> book;
+
+    // write ISBN, number of copies sold, total revenue, and average
+    std::cout << book << std::endl;
+
+    return book;
+}
+
+Sales_item add_two_items()
+{
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2; // read a pair of transactions
+    Sales_item item_res = item1 + item2;
+    std::cout << item_res << std::endl; // print their sum
+    return item_res;
+}
+
+void read_and_output()
+{
+    Sales_item item;
+    while (std::cin >> item)
+    {
+        std::cout << item << std::endl;
+    }
+}
+
+Sales_item read_and_add_two_items()
+{
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2; // read a pair of transactions
+
+    if (item1.isbn() != item2.isbn())
+    {
+        std::cerr << "Error: ISBNs do not match." << std::endl;
+        return Sales_item(); // return a default Sales_item in case of error
+    }
+
+    Sales_item item_res = item1 + item2;
+    std::cout << item_res << std::endl; // print their sum
+    return item_res;
+}
+
+void read_and_add_same_isbn()
+{
+    Sales_item prev, curr, sum;
+    std::cin >> prev;
+    sum = prev;
+    while (std::cin >> curr)
+    {
+        if (prev.isbn() == curr.isbn())
+        {
+            sum += curr;
+        }
+        else
+        {
+            prev = curr;
+            std::cout << sum << std::endl;
+            sum = prev;
+        }
+    }
+    std::cout << sum << std::endl;
+}
+
+int main()
+{
+    read_and_add_same_isbn();
+    return 0;
+}
